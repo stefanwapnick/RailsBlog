57c7bfe002babe381eb5fcb6e31622660f59a91c
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-11-01 15:57:12 -0500

    Added wireframe for edit actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,14 @@ class ArticlesController < ApplicationController
 
 
   # GET - NEW ARTICLE
+  ####################################################################
   def new
-    puts 'TEST 1 TEST 1 TEST 1 TEST 1 TEST 1 TEST 1'
     @article = Article.new
   end
 
   # POST - NEW ARTICLE
+  ####################################################################
   def create
-    puts 'TEST 2 TEST 2 TEST 2 TEST 2 TEST 2 TEST 2'
     @article = Article.new(article_params)
 
     if @article.save
@@ -20,13 +20,32 @@ class ArticlesController < ApplicationController
     else
       render :action => 'new'
     end
-
   end
 
+  # GET - SHOW ARTICLE
+  ####################################################################
   def show
-    #puts 'TEST 3 TEST 3 TEST 3 TEST 3'
     @article = Article.find(params[:id])
     #render plain: @article.inspect
+  end
+
+  # GET - EDIT ARTICLE
+  ####################################################################
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # POST - EDIT ARTICLE
+  ####################################################################
+  def update
+    # Grab existing article
+    @article = Article.find(params[:id])
+    if @article.update(article_params)      # Update article with params passed in
+      flash[:notice] = 'Article was successfully edited'
+      redirect_to article_path(@article)
+    else
+      render :action => 'edit'
+    end
   end
 
   private
