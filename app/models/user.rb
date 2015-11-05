fc40f4bf06d496d817e8643114bb716cb9214cb4
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-11-05 10:41:36 -0500

    Added users migraitona and basic association with articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum:25}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX} ,length: {maximum:105}
+  before_save {self.email = email.downcase}
 end
