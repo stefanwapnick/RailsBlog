af1f81159227543767cd0d5979183cd3229cfdc0
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-12-16 23:52:39 -0500

    Added ui for category and articles association

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,8 @@ class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
 
   def show
-
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
