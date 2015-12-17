77ac3291d989e310ef9c202240f88e1754e05efa
Stefan Wapnick <stefanwapnick@hotmail.com>
2015-12-16 21:24:50 -0500

    Added migrations and classes to implement many to many relationship between categories and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_many :articles, dependent: :destroy       # dependent : destroy will delete on cascade for users (deleting a user will delete all articles associated with user)
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum:25}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX} ,length: {maximum:105}
   before_save {self.email = email.downcase}
